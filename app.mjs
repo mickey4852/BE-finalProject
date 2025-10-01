07f9f8e409ed3721dccb10ba6258b0d22e8439df
Kuntika Siengsukhon <atgenda@gmail.com>
2025-10-01 08:43:21 +0700

    feat:add auth api

--- app.mjs
+++ app.mjs
@@ -1,23 +1,21 @@
 import express from 'express';
 import pool from './utils/db.mjs';
 import bookRouter from './router/bookrouter.mjs';
+import authRouter from './router/auth.mjs';
+import dotenv from 'dotenv';
 
-const app = express();
-const port = 4000;
+async function init() {
+    dotenv.config();
+    const app = express();
+    const port = 4000;
 
-app.use(express.json());
+    app.use(express.json());
+    app.use('/books', bookRouter);
+    app.use('/auth', authRouter);
 
-app.use('/books', bookRouter);
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}
 
-//API test by get all books limit 10
-app.get('/test', async (req, res) => {
-    const { limit = 10 } = req.query;
-    const books = await pool.query('SELECT * FROM books LIMIT $1', [limit]);
-    res.status(200).json(books.rows);
-});
-
-
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+init();
---
+++ router/auth.mjs
@@ -0,0 +1,83 @@
+import { Router } from 'express';
+import pool from '../utils/db.mjs';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const authRouter = Router();
+
+//authen api register
+authRouter.post('/register', async (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+    }
+
+    //check if user already exists
+    const checkUser = await pool.query('SELECT * FROM "users" WHERE email = $1', [user.email]);
+    if (checkUser.rows.length > 0) {
+        return res.status(400).json({message: 'User already exists'});
+    }
+    //check if password is valid
+    if (user.password.length < 8) {
+        return res.status(401).json({message: 'Password must be at least 8 characters long'});
+    }
+
+    //check if email is valid
+    if (!user.email.includes('@')) {
+        return res.status(402).json({message: 'Invalid email'});
+    }
+
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
+    //insert user into database
+    const result = await pool.query(
+        'INSERT INTO "users" (email, password, firstname, lastname) VALUES ($1, $2, $3, $4)',
+        [
+            user.email, 
+            user.password, 
+            user.firstname, 
+            user.lastname, 
+           
+        ]
+        );
+    
+    res.status(200).json({message: 'User registered successfully'});
+});
+
+//api login
+authRouter.post('/login', async (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password,
+    }
+
+    //check if user exists
+    const checkUser = await pool.query('SELECT * FROM "users" WHERE email = $1', [user.email]);
+    if (checkUser.rows.length === 0) {
+        return res.status(403).json({message: 'User not found'});
+    }
+    //check if password is valid
+    const isPasswordValid = await bcrypt.compare(user.password, checkUser.rows[0].password);
+    if (!isPasswordValid) {
+        return res.status(404).json({message: 'Invalid password'});
+    }
+    //generate token
+    const token = jwt.sign({
+        id: checkUser.rows[0].id, 
+        email: user.email, 
+        firstname: checkUser.rows[0].firstname, 
+        lastname: checkUser.rows[0].lastname
+    }, process.env.JWT_SECRET, {
+        expiresIn: '900000'
+    });
+    res.status(200).json({message: 'User logged in successfully', token});
+
+  
+});
+
+
+export default authRouter;
